9544b119c8024c46515235350ed6e4fa5e7789d1
Denise Baker <denviol@yahoo.com>
2019-10-31 08:20:01 -0500

    added login function

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,16 @@
 import 'bootstrap';
+import firebase from 'firebase';
+import apiKeys from './helpers/apiKeys.json';
 import home from './components/home/home';
+import authData from './helpers/data/authData';
+
 
 import '../styles/main.scss';
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   home.loginButton();
+  authData.checkLoginStatus();
 };
 
 init();
