0c09260975f645f38f27347b2f13cadab9369946
Denise Baker <denviol@yahoo.com>
2019-12-05 11:16:40 -0600

    final styling fixes

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -6,18 +6,25 @@ import allBoards from '../../components/allBoards/allBoards';
 const googleDiv = $('#google');
 const logoutButton = $('#logout-button');
 const boardsDiv = $('#boards');
+const loginButton = $('#login');
+const boardsPage = $('#boards2');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       googleDiv.addClass('hide');
       logoutButton.removeClass('hide');
+      loginButton.addClass('hide');
       boardsDiv.removeClass('hide');
       allBoards.buildTheBoards(user.uid);
+      boardsPage.removeClass('hide');
     } else {
       boardsDiv.addClass('hide');
       logoutButton.addClass('hide');
+      loginButton.removeClass('hide');
+      boardsDiv.addClass('hide');
       googleDiv.removeClass('hide');
+      boardsPage.addClass('hide');
     }
   });
 };
